0f7802ffae625d4a20db05fb3b12a194f67ac5ff
asima-chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2021-09-09 23:34:11 +0600

    [TRENDING PAGE] props sent to child component

---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SingleContent.css';
+
+const SingleContent = (props) => {
+    const {id, poster, title, date, media_type, vote_average} = props;
+    // console.log(props);
+    return (
+        <div>
+            <h1>hi </h1>
+        </div>
+    );
+};
+
+export default SingleContent;
